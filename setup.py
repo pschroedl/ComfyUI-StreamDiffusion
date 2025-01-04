628c9f81abdc4f9b0207bcb556f35a8cbc2d7319
John Mull <john@eliteencoder.net>
2025-01-04 04:26:59 +0000

    bump diffusers for comfystream

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "torchvision==0.16.0",
         "xformers",
         "huggingface-hub==0.25.0",
-        "diffusers==0.30.0",
+        "diffusers==0.30.1",
         "protobuf==4.25.3"
     ],
     python_requires=">=3.10",
